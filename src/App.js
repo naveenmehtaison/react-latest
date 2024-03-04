bc28b02eb582f45a5e5580eb6ae51fb3ba359366
naveenmehtaison <naveenmehta2024@outlook.com>
2024-03-04 13:24:38 +0530

    Location_added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import NewComponent from './componentss/NewComponents';
-import SecondComponent from './componentss/SecondComponents';
+// import SecondComponent from './componentss/SecondComponents';
 
 function App() {
   return (
     <div>
       <h1>Lets get started</h1>
       <NewComponent></NewComponent>
-      <SecondComponent></SecondComponent>
     </div>
   );
 }
--- src/componentss/NewComponents.js
+++ src/componentss/NewComponents.js
@@ -1,5 +1,23 @@
+import './NewComponent.css'
 function NewComponent(){
-    return <h1>Expense Tracker</h1>
+    const expenseDate = new Date(2023, 3, 4)
+    const expenseTittle = "Car Insurance"
+    const expenseAmount = 928.32
+    const LocationOfExpenditure = 'uttrakhnad'
+    return (
+        <div className='expense-item'>
+            {/* <h1>Expense Tracker</h1> */}
+            <div>{expenseDate.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{expenseTittle}</h2>
+                <div className='expense-item__price'>{expenseAmount}</div>
+            </div>
+            <div className='expense-item__description'>
+                <h2>{ LocationOfExpenditure}</h2>
+            </div>
+        </div>
+    
+    )
     
 }
 export default NewComponent
