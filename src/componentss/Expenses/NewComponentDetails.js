daa594db98164b1610852d1031d42e19e69ff991
naveenmehtaison <naveenmehta2024@outlook.com>
2024-03-10 17:40:49 +0530

    adding_delete_button

--- src/componentss/Expenses/NewComponentDetails.js
+++ src/componentss/Expenses/NewComponentDetails.js
@@ -1,5 +1,6 @@
 import './NewComponent.css'
 const  NewComponentDetails=(props)=>{
+
     return(
         <div >
             <div className='expense-item__description'>
@@ -7,10 +8,7 @@ const  NewComponentDetails=(props)=>{
                 <h2>{props.location}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
             </div>
-            {/* <div className='expense-item__description'>
-                
-            </div>           */}
-
+            
     
         </div>
     
--- src/componentss/Expenses/NewComponents.js
+++ src/componentss/Expenses/NewComponents.js
@@ -4,11 +4,22 @@ import NewComponentDetails from './NewComponentDetails.js';
 import Card from '../Ui/card.js';
 
 const NewComponent=(props)=> {
+    const DeleteExpense = () => {
+
+        const prices = document.getElementsByClassName('expense-item__price');
+        for (let i = 0; i < prices.length; i++) {
+            const parentDescription = prices[i].parentElement;
+            parentDescription.removeChild(prices[i]);
+        }
+    }
+    
+    
     return (
         <div className='expense-item'>
             
             <NewComponentDate date={props.date}/>
             <NewComponentDetails amount={props.amount} location={props.location} tittle={props.tittle}/>
+            <button onClick={DeleteExpense}>DeleteExpense</button>
         </div>
 
     );
