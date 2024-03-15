5398a2743c8a744f39ec430b1177521f47ecd6d2
naveenmehtaison <naveenmehta2024@outlook.com>
2024-03-15 11:33:09 +0530

    adding_dropdown_and_removing_unwanted_button

--- src/componentss/Expenses/NewComponents.js
+++ src/componentss/Expenses/NewComponents.js
@@ -3,39 +3,45 @@ import './NewComponent.css';
 import NewComponentDate from './NewComponentDate.js';
 import NewComponentDetails from './NewComponentDetails.js';
 import Card from '../Ui/card.js';
+import NewComponentFilter from './NewComponentFilter.js';
 
 const NewComponent=(props)=> {
-
-    const [tittle, setTittle]=useState(props.tittle)
-    const [tittle2, setTittle2]=useState(props.amount)
-    const clickHandler =()=>{
-        setTittle('updated')
-
+    const[filteredYear, setFilteredYear]=useState('2020')
+    const filterChangeHandler = selectedYear=>{
+        setFilteredYear(selectedYear)
     }
-    const clickHandler2 =()=>{
-        setTittle2('$100')
-        console.log(tittle2,setTittle2)
 
-    }
-    const DeleteExpense = () => {
-        let k = props.amount
+    // const [tittle, setTittle]=useState(props.tittle)
+    // const [tittle2, setTittle2]=useState(props.amount)
+    // const clickHandler =()=>{
+    //     setTittle('updated')
+
+    // }
+    // const clickHandler2 =()=>{
+    //     setTittle2('$100')
+    //     console.log(tittle2,setTittle2)
+
+    // }
+    // const DeleteExpense = () => {
+    //     let k = props.amount
 
 
-        const prices = document.getElementsByClassName('expense-item__price');
-        for (let i = 0; i < prices.length; i++) {
-            const parentDescription = prices[i].parentElement;
-            if(prices[i].textContent==k){
-                parentDescription.removeChild(prices[i]);
-            }
-        }
-    }
+    //     const prices = document.getElementsByClassName('expense-item__price');
+    //     for (let i = 0; i < prices.length; i++) {
+    //         const parentDescription = prices[i].parentElement;
+    //         if(prices[i].textContent==k){
+    //             parentDescription.removeChild(prices[i]);
+    //         }
+    //     }
+    // }
     return (
-        <div className='expense-item'>           
+        <div className='expense-item'>
+            <NewComponentFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>           
             <NewComponentDate date={props.date}/>
-            <NewComponentDetails amount={tittle2} location={props.location} tittle={tittle}/>        
-            <button onClick={DeleteExpense}>DeleteExpense</button><div className='expense-item__price'>{tittle2}</div>
+            <NewComponentDetails amount={props.amount} location={props.location} tittle={props.tittle}/>        
+            {/* <button onClick={DeleteExpense}>DeleteExpense</button><div className='expense-item__price'>{tittle2}</div>
             <button onClick={clickHandler}>ChangeTIttle</button>
-            <button onClick={clickHandler2}>ChangeExpnese</button>
+            <button onClick={clickHandler2}>ChangeExpnese</button> */}
         </div>
 
     );
